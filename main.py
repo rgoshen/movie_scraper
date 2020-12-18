1dd9be8b8b7e475b20572060a65cbb7b089f6593
rgoshen <rick.goshen@gmail.com>
2020-12-18 14:30:30 -0700

    added webscrapper and write to text file

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
+response = requests.get(URL)
+
+eo_webpage = response.text
+
+soup = BeautifulSoup(eo_webpage, 'html.parser')
+
+all_movies = soup.find_all(name="h3", class_="title")
+movies_titles = [movie.getText() for movie in all_movies]
+
+movies = movies_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
